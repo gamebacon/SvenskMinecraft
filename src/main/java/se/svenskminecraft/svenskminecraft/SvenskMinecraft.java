037229157733ae9feeaa512a77ed97217d3db375
William With <william@wizdo.se>
2024-07-16 23:30:57 +0200

    init

---
+++ src/main/java/se/svenskminecraft/svenskminecraft/SvenskMinecraft.java
@@ -0,0 +1,18 @@
+package se.svenskminecraft.svenskminecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SvenskMinecraft extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("Hello world!");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
