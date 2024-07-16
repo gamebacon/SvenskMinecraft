5403bf6f988d1f317321d3eff9d3e8a37effed64
William With <william@wizdo.se>
2024-07-17 01:18:05 +0200

    kick on death

--- src/main/java/se/svenskminecraft/svenskminecraft/Events.java
+++ src/main/java/se/svenskminecraft/svenskminecraft/Events.java
@@ -2,7 +2,6 @@ package se.svenskminecraft.svenskminecraft;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
 import io.papermc.paper.ban.BanListType;
-import org.bukkit.BanEntry;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,7 +9,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.time.Duration;
-import java.time.Instant;
 
 public class Events implements Listener {
 
@@ -23,10 +21,10 @@ public class Events implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         event.getPlayer().sendMessage("Player died!!");
         PlayerProfile profile = event.getPlayer().getPlayerProfile();
-        // BanEntry entry = Bukkit.getBanList(BanListType.PROFILE).getBanEntry(profile);
         Duration duration = Duration.ofDays(7);
-        String msg = String.format("Du har är död, men får en ny chans om %s", duration.toString());
-        Bukkit.getBanList(BanListType.PROFILE).addBan(profile, msg, duration, "");
+        String msg = "Du har är död tills vidare";
+        Bukkit.getBanList(BanListType.PROFILE).addBan(profile, msg, duration, "123");
+        event.getPlayer().kick();
     }
 
 }
