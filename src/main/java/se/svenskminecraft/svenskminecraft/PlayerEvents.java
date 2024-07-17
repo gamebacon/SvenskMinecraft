8525f0df7f08e123c1d7962262f326d5b8ccebec
William With <william@wizdo.se>
2024-07-17 11:02:33 +0200

    add icon

--- src/main/java/se/svenskminecraft/svenskminecraft/PlayerEvents.java
+++ src/main/java/se/svenskminecraft/svenskminecraft/PlayerEvents.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.time.Duration;
