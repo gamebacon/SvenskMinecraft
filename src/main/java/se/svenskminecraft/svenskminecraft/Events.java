ac1acf9a3ff82c13aa04f3f2a9042bc6deb3c145
William With <william@wizdo.se>
2024-07-17 09:19:46 +0200

    kick on death

--- src/main/java/se/svenskminecraft/svenskminecraft/Events.java
+++ src/main/java/se/svenskminecraft/svenskminecraft/Events.java
@@ -4,11 +4,13 @@ import com.destroystokyo.paper.profile.PlayerProfile;
 import io.papermc.paper.ban.BanListType;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.time.Duration;
 
@@ -16,7 +18,12 @@ public class Events implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage("Welcome to the server!");
+        event.getPlayer().sendMessage("Välkommen!");
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent  event) {
+        event.getPlayer().setGameMode(GameMode.SURVIVAL);
     }
 
     @EventHandler
@@ -24,16 +31,11 @@ public class Events implements Listener {
         event.getPlayer().sendMessage("Player died!!");
         PlayerProfile profile = event.getPlayer().getPlayerProfile();
         Duration duration = Duration.ofDays(7);
-        String msg = String.format(
-                "Du har är dött och återupplivas om %s timmar",
-                duration.toHours()
-        );
+        String msg = "Du dog.";
         Bukkit.getBanList(BanListType.PROFILE).addBan(profile, msg, duration, "123");
         Component deathMessage = event.deathMessage();
         PlayerKickEvent.Cause cause = PlayerKickEvent.Cause.BANNED;
-        Component c = deathMessage
-                .appendNewline().append(Component.text(msg));
-
+        Component c = deathMessage.append(Component.text(msg));
         event.getPlayer().kick(c, cause);
     }
 
